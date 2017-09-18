427a919f90be6b8c2cd935fb37ce1a9e34cde62d
Olya Olga <germiona18@gmail.com>
2017-09-19 00:01:08 +0300

    added .js file

---
+++ site/scripts.js
@@ -0,0 +1,15 @@
+
+/*
+
+var result = [], parent = document.getElementByID("Parent");
+for (var i = 0, l = parent.childNodes.length; i < l; i++) 
+    result.push(parent.childNodes[i].childNodes[0].getAttribute("id"));
+*/function f()
+{	/*var array_of_photos = [];
+	var parent = document.getElementsByClassName("thomas_photos");
+	for(var i=0; i<parent.childNodes.length;++i)
+	{
+		document.getElementsByClassName("pageHeader").innerHTML+=i;
+	}*/
+	document.getElementById("current_image").src = "tom_1.jpg";
+}
