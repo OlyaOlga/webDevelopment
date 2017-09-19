28c20b019610a01e3e24a3311335e8693a91ceed
Olya Olga <germiona18@gmail.com>
2017-09-19 11:18:25 +0300

    implemented changing size of image on mouseover and on mouseout

--- site/scripts.js
+++ site/scripts.js
@@ -1,9 +1,21 @@
+
 function rand_photo(id)
 {	
 	var tom = "tom_";
 	var jpg=".jpg";
 	var num = Math.floor(Math.random()*7) +1;
 	var rnd_photo = tom+num+jpg;
-	console.log(rnd_photo);
 	document.getElementById(id).src = rnd_photo;
+}
+
+function change_size(id)
+{
+	document.getElementById(id).style.height = "550px";
+	document.getElementById(id).style.width = "400px";
+}
+
+function return_orig_size(id)
+{
+	document.getElementById(id).style.height = "515px";
+	document.getElementById(id).style.width = "386px";
 }
