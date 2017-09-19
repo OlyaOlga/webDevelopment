aa94aa2019d68ff9c810785d7f17ef6422b5fadd
Olya Olga <germiona18@gmail.com>
2017-09-19 10:24:24 +0300

    changes current photo to random photo

--- site/scripts.js
+++ site/scripts.js
@@ -1,15 +1,9 @@
-
-/*
-
-var result = [], parent = document.getElementByID("Parent");
-for (var i = 0, l = parent.childNodes.length; i < l; i++) 
-    result.push(parent.childNodes[i].childNodes[0].getAttribute("id"));
-*/function f()
-{	/*var array_of_photos = [];
-	var parent = document.getElementsByClassName("thomas_photos");
-	for(var i=0; i<parent.childNodes.length;++i)
-	{
-		document.getElementsByClassName("pageHeader").innerHTML+=i;
-	}*/
-	document.getElementById("current_image").src = "tom_1.jpg";
+function rand_photo(id)
+{	
+	var tom = "tom_";
+	var jpg=".jpg";
+	var num = Math.floor(Math.random()*7) +1;
+	var rnd_photo = tom+num+jpg;
+	console.log(rnd_photo);
+	document.getElementById(id).src = rnd_photo;
 }
