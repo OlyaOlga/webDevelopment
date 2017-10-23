f00284e50162b962ea49650433fc13ebf5a1e3c0
Olya Olga <germiona18@gmail.com>
2017-10-23 23:59:15 +0300

    Added user bucket, added admin interface and possibility to delete items

--- OnlineBookshop/server/routes/index.js
+++ OnlineBookshop/server/routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 var app = express();
 var session = session = require('express-session');
 const router = express.Router();
-//var cookieParser = require('cookie-parser');
 const pg = require('pg');
 const path = require('path');
 var connectionString = "postgres://postgres:ll1745ll@127.0.0.1:5432/site";
@@ -82,7 +81,7 @@ app.get('/catalog', function(req, res){
 				  return res.status(500).json({success: false, data: err});
 				}
 				console.log('query start');
-				const query = client.query("SELECT * FROM catalog");
+				const query = client.query("SELECT * FROM catalogue");
 				query.on('row', (row) => {
 					rows.push(row);
 					console.log(row);
@@ -94,5 +93,158 @@ app.get('/catalog', function(req, res){
 				});
 			});
 });
+
+app.get('/buy',function(req, res){
+	console.log(req.session);
+	if(!req.session.user)
+	{
+		console.log("user not authorised");
+		res.render('error', {user:req.session.user,message: 'You are not logged in'});
+		return;
+	}
+	if(!req.session.admin)
+	{
+			console.log("ordinary user");
+		if(req.query.id)
+		{
+				pg.connect(connectionString, (err, client, done) => 
+				{
+					if(err)
+					{
+					  done();
+					  console.log(err);
+					  return res.status(500).json({success: false, data: err});
+					}
+					console.log('query start');
+					var q = "SELECT * FROM catalogue WHERE id = " + req.query.id;
+					console.log(q);
+					const query = client.query("SELECT * FROM catalogue WHERE id = " + req.query.id);
+					query.on('row', (row) => 
+					{
+						req.session.store.push(row);
+						console.log(row);
+					});
+					
+					query.on('end', () => 
+					{									
+						done();
+						var sum=0;
+						for(var i=0; i<req.session.store.length;++i)
+						{
+							sum+=req.session.store[i].price;
+							console.log("total sum: "+sum+"  "+i+" length: "+ req.session.store.length);
+						}
+						res.render('buy',{user:req.session.user,tovar:req.session.store, totalSum:sum});
+					});
+				});
+		}
+		else
+		{
+			var sum=0;
+			for(var i=0; i<req.session.store.length;++i)
+					{
+						sum+=req.session.store[i].price;
+						console.log("total sum: "+sum+"  "+i+" length: "+ req.session.store.length);
+					}
+				res.render('buy',{user:req.session.user,tovar:req.session.store,totalSum:sum});
+		}
+	}	
+	else
+	{
+		console.log("ADMIN!!!");
+		if(req.query.action == "remove" && req.query.id)
+		{
+			pg.connect(connectionString, (err, client, done) => {
+			if(err) {
+			  done();
+			  console.log(err);
+			  return res.status(500).json({success: false, data: err});
+			}
+			console.log('query start');
+			const query = client.query("delete from catalogue where id = " + req.query.id);
+			query.on('row', (row) => {});				
+			query.on('end', () => 
+				{
+					done();
+					loadAdm(req,res);
+				});
+			});	
+		}		
+		else
+		{
+			if(req.query.action == "change" && req.query.id)
+			{
+				var temp;
+			pg.connect(connectionString, (err, client, done) => {//!!!!!!!!!!!!
+			if(err) 
+			{
+			  done();
+			  console.log(err);
+			  return res.status(500).json({success: false, data: err});
+			}
+			console.log('query start');
+			const query = client.query("select * from catalogue where id="+req.query.id);
+			query.on('row', (row) =>
+			{
+				temp = row;
+			});				
+			query.on('end', () => 
+			{
+				done();
+				res.render('change', {user:req.session.user, rowResult:temp});
+			});			
+			});
+			}
+			else{
+				loadAdm(req,res);
+			}
+		
+		}
+	}
+});
+
+app.post('/change', function(req, res){
+	
+	pg.connect(connectionString, (err, client, done) => {
+		if(err) {
+		  done();
+		  console.log(err);
+		  return res.status(500).json({success: false, data: err});
+		}
+		client.query('update catalogue set  name=\''+ req.body.ChangedName+
+					'\', price=\''+req.body.ChangedPrice+'\', description=\''+
+					req.body.ChangedDescription+'\', img=\''+req.body.ChangedPhoto+
+					'\' where id='+5);
+		res.send('update catalogue set  name=\''+ req.body.ChangedName+
+					'\', price=\''+req.body.ChangedPrice+'\', description=\''+
+					req.body.ChangedDescription+'\', img=\''+req.body.ChangedPhoto+
+					'\' where id='+5);
+		//update catalogue set  name='Alchemist', description='some very short description', img='', price='100' where id=5;
+	
+});
+});
+function loadAdm(req,res)
+{
+	const rows = [];
+		pg.connect(connectionString, (err, client, done) => {
+			if(err)
+			{
+			  done();
+			  console.log(err);
+			  return res.status(500).json({success: false, data: err});
+			}
+			console.log('query start');
+			const query = client.query("SELECT * FROM catalogue");
+			query.on('row', (row) => {
+				rows.push(row);
+				console.log(row);
+			});				
+			query.on('end', () => {
+			  done();
+			  console.log(rows.length);
+			  res.render('admin', {user:req.session.user, tovar:rows});
+			});
+		});
+}
 
 module.exports = app;
