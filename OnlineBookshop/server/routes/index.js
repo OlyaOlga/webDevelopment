b3654e98df37694ebeecdb52ae3daf8fe432424e
Olya Olga <germiona18@gmail.com>
2017-10-30 22:59:48 +0200

    implemented user login

--- OnlineBookshop/server/routes/index.js
+++ OnlineBookshop/server/routes/index.js
@@ -24,10 +24,11 @@ app.get('/', function(req, res) //localhost:4000/      <-------
 });
 
 app.get('/login', function(req,res){
-	res.render('login', {message: "", user:req.session.user});
+	res.render('login', {message: "",registerMessage:"", user:req.session.user});
 });
 
 app.post('/login', function(req,res){
+	
 	var auth;
 		pg.connect(connectionString, (err, client, done) => {
 		if(err) {
@@ -55,10 +56,54 @@ app.post('/login', function(req,res){
 		  }
 		  else
 		  {
-			  res.render('login', {user:req.session.user, message: 'Authentication failed'});
+			  res.render('login', {user:req.session.user, message: 'Authentication failed', registerMessage:""});
 		  }
 		});
 	});
+});
+
+app.post('/register', function(req,res){
+	pg.connect(connectionString, (err, client, done) => {
+		if(err) {
+		  done();
+		  console.log(err);
+		  return res.status(500).json({success: false, data: err});
+		}
+		
+		var usernameExists = false;
+		const query = client.query('select count(username) as quantity from users where username = \'' + req.body.newUser+ '\'');
+				query.on('row', (row) => {
+					console.log(row.quantity);
+					if(row.quantity == 1)
+					{
+					usernameExists = true;
+					console.log(req.body.newUser+' already exists');
+					res.render('login', {user:req.session.user, message: '', registerMessage:'User with this login already exists. Please choose another'});
+					}
+				});
+				query.on('end', () => {
+				  done();
+				  if(usernameExists==false)
+				  {
+				  		const query2 = client.query('insert into users values (\''+ req.body.newUser + '\', \''+req.body.newPassword +'\', false);');
+				query2.on('row', (row) => {
+					console.log(req.body.newUser);
+					
+				});
+				query2.on('end', () => {
+				  done();
+				  req.session.user=req.body.newUser;
+				  res.render('main', {user:req.session.user});
+				});
+				  }
+				  else{
+					  res.render('main', {user:req.session.user});
+				  }
+		
+			});
+		
+		
+		});
 });
 
 app.get('/admin', function(req,res){
@@ -73,6 +118,8 @@ app.get('/admin', function(req,res){
 });
 
 app.get('/catalog', function(req, res){
+	if(!req.session.admin)
+	{
 	const rows = [];
 			pg.connect(connectionString, (err, client, done) => {
 				if(err) {
@@ -91,7 +138,61 @@ app.get('/catalog', function(req, res){
 				  console.log(rows.length);
 				  res.render('catalog', {user:req.session.user, catalog:rows});
 				});
+			}); 
+	}
+	else
+	{
+		console.log("ADMIN!!!");
+		if(req.query.action == "remove" && req.query.id)
+		{
+			pg.connect(connectionString, (err, client, done) => {
+			if(err) {
+			  done();
+			  console.log(err);
+			  return res.status(500).json({success: false, data: err});
+			}
+			console.log('query start');
+			const query = client.query("delete from catalogue where id = " + req.query.id);
+			query.on('row', (row) => {});				
+			query.on('end', () => 
+				{
+					done();
+					loadAdm(req,res);
+				});
+			});	
+		}		
+		else
+		{
+			if(req.query.action == "change" && req.query.id)
+			{
+				var temp;
+			pg.connect(connectionString, (err, client, done) => {//!!!!!!!!!!!!
+			if(err) 
+			{
+			  done();
+			  console.log(err);
+			  return res.status(500).json({success: false, data: err});
+			}
+			console.log('query start');
+			const query = client.query("select * from catalogue where id="+req.query.id);
+			query.on('row', (row) =>
+			{
+				temp = row;
+			});				
+			query.on('end', () => 
+			{
+				done();
+				console.log("ID on admin's page: "+temp.id);
+				res.render('change', {user:req.session.user, rowResult:temp});
+			});			
 			});
+			}
+			else
+			{
+				loadAdm(req,res);
+			}
+		}
+	}
 });
 
 app.get('/buy',function(req, res){
@@ -99,7 +200,7 @@ app.get('/buy',function(req, res){
 	if(!req.session.user)
 	{
 		console.log("user not authorised");
-		res.render('error', {user:req.session.user,message: 'You are not logged in'});
+		res.render('error', {user:req.session.user, message: 'You are not logged in'});
 		return;
 	}
 	if(!req.session.admin)
@@ -151,60 +252,32 @@ app.get('/buy',function(req, res){
 	}	
 	else
 	{
-		console.log("ADMIN!!!");
-		if(req.query.action == "remove" && req.query.id)
-		{
+		const rows = [];
 			pg.connect(connectionString, (err, client, done) => {
-			if(err) {
-			  done();
-			  console.log(err);
-			  return res.status(500).json({success: false, data: err});
-			}
-			console.log('query start');
-			const query = client.query("delete from catalogue where id = " + req.query.id);
-			query.on('row', (row) => {});				
-			query.on('end', () => 
-				{
-					done();
-					loadAdm(req,res);
+				if(err) {
+				  done();
+				  console.log(err);
+				  return res.status(500).json({success: false, data: err});
+				}
+				console.log('query start');
+				const query = client.query("select order_id, username, name, price from catalogue, orders where book_num=id;");
+				query.on('row', (row) => {
+					rows.push(row);
+					console.log(row);
 				});
-			});	
-		}		
-		else
-		{
-			if(req.query.action == "change" && req.query.id)
-			{
-				var temp;
-			pg.connect(connectionString, (err, client, done) => {//!!!!!!!!!!!!
-			if(err) 
-			{
-			  done();
-			  console.log(err);
-			  return res.status(500).json({success: false, data: err});
-			}
-			console.log('query start');
-			const query = client.query("select * from catalogue where id="+req.query.id);
-			query.on('row', (row) =>
-			{
-				temp = row;
-			});				
-			query.on('end', () => 
-			{
-				done();
-				res.render('change', {user:req.session.user, rowResult:temp});
-			});			
-			});
-			}
-			else{
-				loadAdm(req,res);
-			}
-		
-		}
+				query.on('end', () => {
+				  done();
+				  console.log(rows.length);
+				  res.render('all_orders', {user:req.session.user, orders:rows});
+				});
+			}); 		
 	}
 });
 
-app.post('/change', function(req, res){
+app.post('/change', function(req, res)
+{
 	
+console.log(req.body.ChangedPrice+"    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 	pg.connect(connectionString, (err, client, done) => {
 		if(err) {
 		  done();
@@ -214,13 +287,8 @@ app.post('/change', function(req, res){
 		client.query('update catalogue set  name=\''+ req.body.ChangedName+
 					'\', price=\''+req.body.ChangedPrice+'\', description=\''+
 					req.body.ChangedDescription+'\', img=\''+req.body.ChangedPhoto+
-					'\' where id='+5);
-		res.send('update catalogue set  name=\''+ req.body.ChangedName+
-					'\', price=\''+req.body.ChangedPrice+'\', description=\''+
-					req.body.ChangedDescription+'\', img=\''+req.body.ChangedPhoto+
-					'\' where id='+5);
-		//update catalogue set  name='Alchemist', description='some very short description', img='', price='100' where id=5;
-	
+					'\' where id='+req.body.id);
+					loadAdm(req, res);	
 });
 });
 function loadAdm(req,res)
@@ -246,5 +314,7 @@ function loadAdm(req,res)
 			});
 		});
 }
+
+
 
 module.exports = app;
